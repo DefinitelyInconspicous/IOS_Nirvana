6fdabbf4c5a88bbff6db569e9be96f1bd0d82ec4
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2025-02-17 07:24:17 +0800

    Initial Commit

---
+++ IOS_Nirvana/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS_Nirvana
+//
+//  Created by Avyan Mehra on 17/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS_Nirvana/IOS_NirvanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_NirvanaApp.swift
+//  IOS_Nirvana
+//
+//  Created by Avyan Mehra on 17/2/25.
+//
+
+import SwiftUI
+
+@main
+struct IOS_NirvanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
